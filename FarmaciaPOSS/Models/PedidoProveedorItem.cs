866646e677ffe42030d776d21b9f0d5fb51ad0cc
frediad <frediadame1@gmail.com>
2026-07-27 20:28:17 -0600

    pedidos

--- FarmaciaPOSS/Models/PedidoProveedorItem.cs
+++ FarmaciaPOSS/Models/PedidoProveedorItem.cs
@@ -1,16 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace FarmaciaPOS.Models
 {
-    public class PedidoProveedorItem
+    public class PedidoProveedorItem : INotifyPropertyChanged
     {
+        private int cantidad;
+        private decimal costoUnitario;
+
         public string Nombre { get; set; } = string.Empty;
-        public int Cantidad { get; set; }
-        public decimal CostoUnitario { get; set; }
+
+        public int Cantidad
+        {
+            get => cantidad;
+            set
+            {
+                cantidad = value;
+                OnPropertyChanged(nameof(Cantidad));
+                OnPropertyChanged(nameof(Subtotal));
+            }
+        }
+
+        public decimal CostoUnitario
+        {
+            get => costoUnitario;
+            set
+            {
+                costoUnitario = value;
+                OnPropertyChanged(nameof(CostoUnitario));
+                OnPropertyChanged(nameof(Subtotal));
+            }
+        }
+
         public decimal Subtotal => Cantidad * CostoUnitario;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
